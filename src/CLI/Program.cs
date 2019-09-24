49ff32250abc34460c6be0ff4682541d68e94592
CassandraD <2866070+Cassandra-d@users.noreply.github.com>
2019-09-24 21:13:33 +0300

    Added three different project types

---
+++ src/CLI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CLI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var dispossable = new System.IO.StreamWriter("file.txt", append: true);
+            await dispossable.WriteLineAsync();
+        }
+    }
+}
---
+++ src/Lib/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Lib
+{
+    public class Class1<T>
+    {
+        public static T SomeProperty { get; set; }
+    }
+}
